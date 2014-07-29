613e83fc683363c88c0a7cb01ab3d3606a20f739
Alessandro Gnoli <tebemis@gmail.com>
2014-07-29 14:13:31 -0500

    Fixed gradle script and finalized "unseal"

--- nutella-cli/commands/unseal.rb
+++ nutella-cli/commands/unseal.rb
@@ -4,14 +4,27 @@ class Unseal < Command
   @description = "Checks that all the dependencies are installed and prepares nutella to run"
   
   def run(args=nil)
+    # TODO Whenever we install a dependency we need to write in the config file
+    # TODO check all the dependencies before installing anything, git, ruby, npm,
+    # TODO and all the associated configurations
+    # TODO Execute this command from Travis to verify installation
+    
     # Check if we have a broker and installs one if not
-    if !nutella.broker.has_key?(:exist)
+    if !nutella.dependencies.broker.has_key?(:exist)
       installBroker
     end  
   end
   
-  def installBroker
-    puts "broker not installed"
+  def installBroker    
+    # Clone, cd and npm install
+    install_dir = "#{nutella.home_dir}/deps/mosca"
+    system "git clone git://github.com/mcollina/mosca.git #{install_dir}"
+    Dir.chdir(install_dir)
+    system "npm install"
+    
+    # Add startup script
+    File.open("startup", 'w') { |file| file.write("#!/bin/sh\n\nBASEDIR=$(dirname $0)\n$BASEDIR/bin/mosca --http-port 1884 -v\n") }
+    File.chmod(0755, "startup")
   end
   
 end
