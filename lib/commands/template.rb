ab9852db9f18810c4b3d8ebd3df82c2024b56a10
Alessandro Gnoli <tebemis@gmail.com>
2015-03-12 10:50:09 -0500

    Cleaned up template a bit

--- lib/commands/template.rb
+++ lib/commands/template.rb
@@ -45,73 +45,92 @@ module Nutella
     end
 
 
-    def create_template_sub_command( name_d )
-      ver_d = '0.1.0'
-      type_d = 'bot'
-
-      # Name
-      puts 'What is the name of your template?'
-      print "(#{name_d}) "
-      c = $stdin.gets.chomp!
-      name = c.empty? ? name_d : c
-      # Version
-      puts 'What is the version of your template?'
-      print "(#{ver_d}) "
-      c = $stdin.gets.chomp!
-      version = c.empty? ? ver_d : c
-      # Type
-      puts 'Are you creating a template for a "bot" or "interface"?'
-      print "(#{type_d}) "
-      c = $stdin.gets.chomp!
-      type = c.empty? ? type_d : c
-      # Description
-      puts "Do you want to provide a short description for your #{type} template?"
-      print '(optional, hit enter if no description) '
-      description = $stdin.gets.chomp!
-      # Repo
-      puts 'Do you want to provide a git repository for your template?'
-      print '(optional, hit enter if no repo) '
-      repo = $stdin.gets.chomp!
-
-      # Build JSON and show confirmation
-      puts 'Looks good?'
+    def create_template_sub_command( template_name )
+      @default_version = '0.1.0'
+      @default_type = 'bot'
+      # Get template parameters from command line
+      name = prompt_and_read_name template_name
+      version = prompt_and_read_version
+      type = prompt_and_read_type
+      description = prompt_and_read_description type
+      repo = prompt_and_read_repo
+      # Build JSON
       json = build_nutella_json( name, version, type, description, repo )
-      puts JSON.pretty_generate json
-      print '(yes/no) '
-
-      # If user confirms, create template
-      confirm = $stdin.gets.chomp!
-      if confirm=='yes'
-        create_template_files json
-      else
-        console.warn 'Template creation aborted'
-      end
+      # Show confirmation and read
+      prompt_and_read_confirm json
 
     end
 
 
-    def create_template_files( json )
+    def prompt_and_read_name(template_name)
+      puts 'What is the name of your template?'
+      print "(#{template_name}) "
+      c = $stdin.gets.chomp!
+      c.empty? ? template_name : c
+    end
+
+    def prompt_and_read_version
+      puts 'What is the version of your template?'
+      print "(#{ver_d}) "
+      c = $stdin.gets.chomp!
+      c.empty? ? ver_d : c
+    end
+
+    def prompt_and_read_type
+      puts 'Are you creating a template for a "bot" or "interface"?'
+      print "(#{type_d}) "
+      c = $stdin.gets.chomp!
+      c.empty? ? type_d : c
+    end
+
+    def prompt_and_read_description(type)
+      puts "Do you want to provide a short description for your #{type} template?"
+      print '(optional, hit enter if no description) '
+      $stdin.gets.chomp!
+    end
+
+    def prompt_and_read_repo
+      puts 'Do you want to provide a git repository for your template?'
+      print '(optional, hit enter if no repo) '
+      $stdin.gets.chomp!
+    end
+
+    def prompt_and_read_confirm(json)
+      puts 'Looks good?'
+      puts JSON.pretty_generate json
+      print '(yes/no) '
+      confirm = $stdin.gets.chomp!
+      if confirm=='yes'
+        create_template json
+      else
+        console.warn 'Template creation aborted'
+      end
+    end
+
+    def create_template( json )
       # First validate the JSON
       unless validate_nutella_file_json json
         console.error 'Something was wrong with your nutella.json file. Template creation aborted'
         return
       end
-      # Assemble destination directory
+      # Check that the template directory doesn't already exist
       template_dir = File.join( Dir.pwd, json['name'] )
-      # Check that the directory doesn't exist already
-      if File.directory?(template_dir)
+      if File.directory? template_dir
         console.error("The directory #{template_dir} already exists! Can't create template #{json[:name]}")
         return
       end
+      # Create template directory and files
+      create_template_files(json, template_dir)
+    end
+
+    def create_template_files(json, template_dir)
       # Create directory
       Dir.mkdir template_dir
       # Create nutella.json file
       File.open("#{template_dir}/nutella.json", 'w') { |f| f.write(JSON.pretty_generate json) }
       # Add bot/interface specific files
-      bot_specific_file = nil
-      bot_specific_file = File.join( Nutella::NUTELLA_HOME, 'data/startup' ) if json['type']=='bot'
-      bot_specific_file = File.join( Nutella::NUTELLA_HOME, 'data/index.html' ) if json['type']=='interface'
-      FileUtils.copy( bot_specific_file, template_dir )
+      FileUtils.copy(File.join(Nutella::NUTELLA_HOME, 'data/startup'), template_dir) if json['type']=='bot'
+      FileUtils.copy(File.join(Nutella::NUTELLA_HOME, 'data/index.html'), template_dir) if json['type']=='interface'
       console.success "Template #{json['name']} created successfully!"
     end
 
