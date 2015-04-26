e4db3f9aaa5de50b68d533862ee25575c1c3b02c
Alessandro Gnoli <tebemis@gmail.com>
2015-04-26 16:03:59 -0500

    Fixed framework level persistence APIs

--- nutella_lib/framework_persist.rb
+++ nutella_lib/framework_persist.rb
@@ -12,6 +12,8 @@ module Nutella
     # Implements basic persistence for framework-level components
     module Persist
 
+      # @!group Framework-level APIs
+
       # This method returns a MongoDB-backed store (i.e. persistence)
       # for a collection (i.e. an Array)
       # @param [String] name the name of the store
@@ -49,6 +51,62 @@ module Nutella
         FileUtils.mkdir_p dir_path
         JSONFilePersistedHash.new file_path
       end
+
+      # @!endgroup
+
+      # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+      # @!group Run-level APIs
+
+      # This method returns a MongoDB-backed store
+      # for a collection at the run level
+      # @param [String] app_id
+      # @param [String] run_id
+      # @param [String] name the name of the store
+      # @return [MongoPersistedCollection] a MongoDB-backed collection store
+      def self.get_run_mongo_collection_store( app_id, run_id, name )
+        MongoPersistedCollection.new Nutella.mongo_host, app_id, "#{run_id}/#{name}"
+      end
+
+      # This method returns a MongoDB-backed store
+      # for a single object at the run level
+      # @param [String] app_id
+      # @param [String] run_id
+      # @param [String] name the name of the store
+      # @return [MongoPersistedHash] a MongoDB-backed Hash store
+      def self.get_run_mongo_object_store( app_id, run_id, name )
+        MongoPersistedHash.new Nutella.mongo_host, app_id, 'run_persisted_hashes', "#{run_id}/#{name}"
+      end
+
+      # This method returns a JSON-file-backed store
+      # for a collection at the run level
+      # @param [String] app_id
+      # @param [String] run_id
+      # @param [String] name the name of the store
+      # @return [JSONFilePersistedCollection] a JSON-file-backed collection store
+      def self.get_run_json_collection_store( app_id, run_id, name )
+        dir_path = "#{ENV['HOME']}/.nutella/data/#{Nutella.component_id}/#{app_id}/#{run_id}"
+        file_path = "#{dir_path}/#{name}.json"
+        FileUtils.mkdir_p dir_path
+        JSONFilePersistedCollection.new file_path
+      end
+
+      # This method returns a JSON-file-backed store
+      # for a single object at the run level
+      # @param [String] app_id
+      # @param [String] run_id
+      # @param [String] name the name of the store
+      # @return [JSONFilePersistedHash] a JSON-file-backed Hash store
+      def self.get_run_json_object_store( app_id, run_id, name )
+        dir_path = "#{ENV['HOME']}/.nutella/data/#{Nutella.component_id}/#{app_id}/#{run_id}"
+        file_path = "#{dir_path}/#{name}.json"
+        FileUtils.mkdir_p dir_path
+        JSONFilePersistedHash.new file_path
+      end
+
+
+      # @!endgroup
+
     end
   end
 
