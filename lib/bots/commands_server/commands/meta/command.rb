23ec4c18cefafa7e16c9aa3ab0405c0f6f80b4ec
Alessandro Gnoli <tebemis@gmail.com>
2019-10-26 17:17:05 -0700

    Move commands around

---
+++ lib/bots/commands_server/commands/meta/command.rb
@@ -0,0 +1,17 @@
+module Nutella
+
+  # Nutella command
+  class Command 
+
+    class << self;
+      attr_accessor :description
+    end
+  
+    # Commands overload this method to execute
+    def run( args=nil )
+      console.error 'Running the generic command!!! WAT? https://www.destroyallsoftware.com/talks/wat'
+    end
+
+  end
+
+end
