9ab34255c33d64dc01e00362ef26ddb8314f0fc8
Alessandro Gnoli <tebemis@gmail.com>
2019-10-27 18:26:00 -0700

    Cleanup for the weekend

--- lib/supervisor/supervisor.rb
+++ lib/supervisor/supervisor.rb
@@ -2,29 +2,20 @@
 
 require_relative 'supervisor_services_pb'
 
+# Defines the gRPC server
 class SupervisorServer < Supervisor::Service
-  # say_hello implements the SayHello rpc method.
+  # start_process implements the StartProcess rpc method.
   def start_process(start_process_req, _unused_call)
     puts "Someone requested to start a process!!! #{start_process_req}"
     res = StartProcessResponse.new(success: true)
     puts "Responding with: #{res}"
+    raise "ALE IS AN ASS"
     res
   end
 end
 
-# Everything that is not an SIGINT (2), SIGTERM (15), 
-# or a hard SIGKILL (9) (and a SIGSTOP(19) I guess...)
-# will trigger a process restart
-begin
-  # Listen for commands over GRPC
-  # Start and stop processes
-  # Persist after each operation
-  s = GRPC::RpcServer.new
-  s.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
-  s.handle(SupervisorServer)
-  s.run_till_terminated_or_interrupted([1, 'int', 'SIGQUIT'])
-rescue SignalException => e
-  # Legit termination, exit
-rescue
-  # everything else... restart
-end
+# Runs the gRPC server
+s = GRPC::RpcServer.new
+s.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
+s.handle(SupervisorServer)
+s.run_till_terminated_or_interrupted([1, 'int', 'SIGQUIT'])
